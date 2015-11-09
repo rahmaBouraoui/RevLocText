b4b6cf32f98c08ccb51b21d675cfc55cb94ab391
bouraoui <bouraoui.rahma.sio@gmail.com>
2015-11-09 17:09:16 +0100

    c3

--- Garagiste.java
+++ Garagiste.java
@@ -3,5 +3,10 @@ public class Garagiste {
 	private String nom ;
 	private String adresse ;
 
+	public Garagiste(String nom, String adresse) {
+		this.nom = nom ;
+		this.adresse = adresse ;
+	}
+
 }
 
